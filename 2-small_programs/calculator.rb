0cd9356299bef61fceb8fae0822b69fab42d0f35
Ying Chyi Gooi <Ying Chyi Gooi>
2017-03-31 10:29:53 -0500

    sixth-commit

--- 2-small_programs/calculator.rb
+++ 2-small_programs/calculator.rb
@@ -1,3 +1,8 @@
+require 'pry'
+require 'yaml'
+LANG = 'en'
+MSG = YAML.load_file('calc_msg.yml')
+
 # ask the user for two numbers
 # ask the user for an operation to perform
 # perform the operation on the two numbers
@@ -6,25 +11,113 @@
 # answer = Kernel.gets()
 # Kernel.puts(answer)
 
-Kernel.puts("Welcome to Calculator!")
-
-Kernel.puts("What's the first number?")
-num1 = Kernel.gets().chomp()
-
-Kernel.puts("What's the second number?")
-num2 = Kernel.gets().chomp()
-
-Kernel.puts("What operation would you like to perform? 1) add 2) subract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
-
-if operator == '1'
-  result = num1.to_i() + num2.to_i() #if condition does not create a block, therefore the variable created in if is visible outside the if condition
-elsif operator == '2'
-  result = num1.to_i() - num2.to_i()
-elsif operator == '3'
-  result = num1.to_i() * num2.to_i()
-else
-  result = num1.to_f() / num2.to_f()                 
+def messages(msg, lang='en')
+  MSG[lang][msg]
 end
 
-Kernel.puts("The result is #{result}")
+def prompt(msg)
+  Kernel.puts("=> #{msg}")
+end
+
+def integer?(num)
+  num.to_i.to_s == num
+end
+
+def float?(num)
+  /\d/.match(num) && /^\d*\.?\d*$/.match(num)
+end
+
+def valid_number?(num)
+  integer?(num) || float?(num)
+end
+
+def operation_to_msg(op)
+  word = case op
+          when '1'
+            'Adding'
+          when '2'
+            'Subtracting'
+          when '3'
+            'Multiplying'
+          when '4'
+            'Dividing'
+          end
+
+  word
+end
+
+prompt(messages('welcome',LANG))
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?
+    prompt(messages('valid_name',LANG))
+  else
+    break
+  end
+end
+
+num1 = ''
+num2 = ''
+operator = ''
+
+prompt("Hi, #{name}!")
+
+loop do
+  loop do
+    prompt(messages('input1', LANG))
+    num1 = Kernel.gets().chomp() #the scope of num1 is not visible outside the loop block
+
+    if valid_number?(num1)
+      break
+    else
+      prompt(messages('valid_number', LANG))
+    end
+  end
+
+  loop do
+    prompt(messages('input2', LANG))
+    num2 = Kernel.gets().chomp()
+
+    if valid_number?(num2)
+      break
+    else
+      prompt(messages('valid_number', LANG))
+    end
+  end
+
+  prompt(messages('operator_prompt', LANG))
+
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include? operator
+      break
+    else
+      prompt(messages('valid_selection', LANG))
+    end
+  end
+
+  prompt "#{operation_to_msg(operator)} the two numbers..."
+
+  result =  case operator
+            when '1'
+              result = num1.to_f() + num2.to_f()
+            when '2'
+              result = num1.to_f() - num2.to_f()
+            when '3'
+              result = num1.to_f() * num2.to_f()
+            when '4'
+              result = num1.to_f() / num2.to_f()
+            end
+
+  prompt "The result is #{result}"
+
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("#{name}, Thank you for using the calculator!")
---
+++ 2-small_programs/mortgage.rb
@@ -0,0 +1,108 @@
+# mortgage.rb
+
+# a simple mortgage calculator to calculate monthly payments
+
+require 'pry'
+require 'yaml'
+LANG = 'en'
+MSG = YAML.load_file('mortgage_msg.yml')
+
+# Prompt user for inputs:
+# p = the loan amount
+# i = APR
+# yr = n/12 = loan duration in years
+
+# Then we convert some of the inputs into:
+# monthly interest rate (j) = APR/12 = i/12
+# loan_duration_months (n) = loan_duration_years*12 = y * 12
+
+# Using the PV annuity formula,
+# we calculate and output the monthly payment (m) to the user
+
+def prompt(msg) # using a shorthand parser here
+  puts("=> #{MSG[LANG][msg]}")
+end
+
+def output(msg)
+  print("  => #{MSG[LANG][msg]}")
+end
+
+def integer?(num)
+  num.to_i.to_s == num && num.to_i >= 0
+end
+
+def float?(num)
+  /\d/.match(num) && /^\d*\.?\d*$/.match(num)
+end
+
+def valid_number?(num)
+  integer?(num) || float?(num)
+end
+
+# START
+prompt("WELCOME")
+
+loop do
+  p = "" # loan amount
+  i = "" # APR
+  yr = "" # loan duration in years
+  n = nil # loan duration in months = yr * 12
+  j = nil # monthly interest rate = i / 12
+
+  loop do # prompt user input for the loan amount
+    prompt("LOAN_INPUT")
+    p = Kernel.gets().chomp()
+
+    if valid_number?(p)
+      p = p.to_f
+      break
+    else
+      prompt("VALID_NUM")
+    end
+  end
+
+  loop do # prompt user input for APR
+    prompt("APR_INPUT")
+    apr = gets.chomp
+
+    if valid_number?(apr)
+      i = apr.to_f / 100 # convert % into decimal
+      j = i / 12 # calculate the monthly interest rate
+      break
+    else
+      prompt("VALID_NUM")
+    end
+  end
+
+  loop do
+    prompt("YEAR_INPUT")
+    yr = gets.chomp
+
+    if valid_number?(yr)
+      yr = yr.to_f
+      n = yr * 12 # calculate the duration in months
+      break
+    else
+      prompt("VALID_NUM")
+    end
+  end
+
+  m = p * (j / (1 - (1 + j)**-n)) # now we run the magic formula
+
+  prompt("GIVEN_INPUT")
+  output("P")
+  print("$#{format('%.2f', p)}\n")
+  output("I")
+  print("#{i * 100}%\n")
+  output("YR")
+  print("#{yr} years\n")
+
+  prompt("OUTPUT")
+  puts("  => $#{format('%.2f', m)} per month")
+
+  prompt("REPEAT?")
+  answer = gets.chomp
+  break unless %w(y yes yup ya yep).include?(answer.downcase)
+end
+
+prompt("THANK_YOU")
