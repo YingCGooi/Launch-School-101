fd7222ec53db165ef088fe91e6f05dea0207bba6
Ying Chyi Gooi <Ying Chyi Gooi>
2017-04-03 22:52:45 -0500

    added directory 3_exercises

--- 2-small_programs/hello.rb
+++
@@ -1,3 +0,0 @@
-words = %w(scooby doo on channel two)
-words.each {|str| words.delete(str)}
-puts words.inspect  
---
+++ 2_small_programs/hello.rb
@@ -0,0 +1,8 @@
+def extend_greeting(greeting)
+  greeting + " there"
+end
+
+greeting = "hi"
+greeting = extend_greeting(greeting)
+
+puts greeting
